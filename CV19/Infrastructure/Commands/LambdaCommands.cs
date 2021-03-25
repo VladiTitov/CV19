cb7bafbba7a5860370df48cf1df6ce91fdafdbdb
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-25 09:44:13 +0300

    создал LambdaCommand

--- CV19/Infrastructure/Commands/LambdaCommands.cs
+++ CV19/Infrastructure/Commands/LambdaCommands.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CV19.Infrastructure.Commands.Base;
 
 namespace CV19.Infrastructure.Commands
 {
-    class LambdaCommands
+    class LambdaCommands : Command
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommands(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
     }
 }
