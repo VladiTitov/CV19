bcdfb89f52d89b427a94506d86bb9169d06e5f27
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-25 09:38:56 +0300

    создание команды

--- CV19/Infrastructure/Commands/Base/Command.cs
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace CV19.Infrastructure.Commands.Base
 {
-    class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
