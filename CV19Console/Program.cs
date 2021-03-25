c23980ec54c878743f5d5bd46b7db32962c6a498
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-25 10:46:16 +0300

    работа в тестовой консоли - обкатка работы с данными

--- CV19Console/Program.cs
+++ CV19Console/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync((data_url), HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line.Replace(", ", "- ");
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines().First().Split(',').Skip(4)
+            .Select(a => DateTime.Parse(a, CultureInfo.InvariantCulture)).ToArray();
+
+        private static IEnumerable<(string County, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines().Skip(1).Select(line => line.Split(','));
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+
+                yield return (country_name, province, counts);
+            }
+
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var russia_data = GetData().First(v => v.County.Equals("Belarus", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd.MM} - {count}")));
+
+            Console.ReadLine();
         }
     }
 }
