82c122dc8c0b44dadfb4d76dfbfa34d522fb66c8
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-25 09:12:00 +0300

    добавил vm

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CV19.ViewModels.Base
+{
+    class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
