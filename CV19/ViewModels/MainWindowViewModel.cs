5adc8e78dfc7b3c16941f30db45f60c1a13cac78
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-03-25 09:27:05 +0300

    создание модели для тестовых данных координат

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ViewModel
     {
+        private IEnumerable<DataPoint> _testDataPoints;
+
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _testDataPoints;
+            set => Set(ref _testDataPoints, value);
+        }
     }
 }
